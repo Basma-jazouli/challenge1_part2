6db16db519b5641b89cf126dbaeb0f40e3ebe5b4
basma <bassmaeljazzouli@gmail.com>
2023-09-16 14:10:26 +0100

    challenge1 part2

---
+++ challenge1_part2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int a;
+    printf("saisir un entier positif ");
+    scanf("%d",&a);
+
+
+    if(a % 2==0){
+     printf("pair\n");
+    }
+    else{
+     printf("impair\n");
+    }
+
+
+
+return 0;
+}
